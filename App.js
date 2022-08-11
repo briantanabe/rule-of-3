6436f292513e21cd3172ad28f9d43b4871af5356
David Ludeke <david@ludeke.com>
2022-08-10 23:52:15 -0500

    Add bottom modal

--- App.js
+++ App.js
@@ -5,8 +5,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{flex: 1,justifyContent: 'center'}}>
+      <Text>unguuuddduu</Text>
       <StatusBar style="auto" />
+      </View>
+      <View style={styles.bottomModal}>
+        <Text style={styles.bottomModalText}>Solo</Text>
+        </View>
     </View>
   );
 }
@@ -14,8 +19,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
   },
+  bottomModalText: {
+    fontSize: 25,
+    marginLeft: '5%',
+    fontWeight: "bold"
+  },
+  bottomModal: {
+    backgroundColor: "white",
+    width: '95%',
+    height: '10%',
+    marginBottom: '3%',
+    borderRadius: 10,
+    shadowColor: "black",
+    shadowOpacity: .5,
+    shadowRadius: 40,
+    shadowOffset: {
+      height: 20
+    },
+    alignItems: "flex-start",
+    justifyContent: "center"
+  }
 });
